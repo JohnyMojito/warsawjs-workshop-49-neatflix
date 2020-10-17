b7f61525be61c86b76754aaccd74c0cfbdcf8cd1
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-10-17 19:25:10 +0200

    Prepare emissions

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,8 @@
 <template>
   <div id="app">
-    <ShowsListItem />
+    <div class="shows-list">
+      <ShowsListItem :show="show" :isSummaryShown="isSummaryShown"/>
+    </div>
   </div>
 </template>
 ------------------------------------------------------
@@ -12,6 +14,18 @@ export default {
   components: {
     ShowsListItem
   },
+  data() {
+    return {
+      show: {
+        name: 'True Detective', // tutaj tytuł filmu
+        premiered: 2014, // rok premiery
+        rating: 10, // ocena, 0 - 10
+        imgURL: 'http://static.tvmaze.com/uploads/images/medium_portrait/178/445621.jpg',
+        summary: 'Tak, to jest po prostu świetny serial, no. Bardzo polecam',
+      },
+      isSummaryShown: false
+    }
+  }
   // methods: {},
   // computed: {},
   // watch: {},
@@ -22,7 +36,7 @@ export default {
 }
 </script>
 ------------------------------------------------------
-<style>
+<style lang="scss">
 html,
 body {
   margin: 0;
@@ -36,5 +50,8 @@ body {
   color: #2c3e50;
   height: 100vh;
   width: 100vw;
+  .shows-list {
+    padding: 15px;
+  }
 }
 </style>
--- src/components/ShowsListItem.vue
+++ src/components/ShowsListItem.vue
@@ -14,18 +14,7 @@
 <script>
 export default {
   name: 'ShowsListItem',
-    data() {
-    return {
-      show: {
-        name: 'True Detective', // tutaj tytuł filmu
-        premiered: 2014, // rok premiery
-        rating: 10, // ocena, 0 - 10
-        imgURL: 'http://static.tvmaze.com/uploads/images/medium_portrait/178/445621.jpg',
-        summary: 'Tak, to jest po prostu świetny serial, no. Bardzo polecam',
-      },
-      isSummaryShown: false,
-    }
-  }
+  props: ['show', 'isSummaryShown'],
 }
 </script>
 
