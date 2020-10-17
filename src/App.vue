7b8b20439c742a96e6addc61a0f00f51192b917e
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-10-17 21:45:08 +0200

    Prepare SearchInput

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,23 @@
 <template>
   <div id="app">
-    <ShowsList :shows="shows"/>
+    <SearchInput v-model="searchedPhrase"/>
+    <ShowsList :shows="shows" />
   </div>
 </template>
 ------------------------------------------------------
 <script>
 import ShowsList from '@/components/ShowsList'
+import SearchInput from '@/components/SearchInput'
 
 export default {
   name: 'App',
   components: {
+    SearchInput,
     ShowsList
   },
   data() {
     return {
+      searchedPhrase: '',
       shows: [
         {
           name: 'True Detective',
@@ -85,5 +89,6 @@ body {
   height: 100vh;
   width: 100vw;
   display: flex;
+  flex-direction: column;
 }
 </style>
---
+++ src/components/SearchInput.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="search-input">
+    <form role="search">
+      <input type="text" id="header-search" @input="$emit('input', $event.target.value)"/>
+      <label for="header-search" class="visually-hidden">Search</label>
+    </form>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['value'],
+  name: 'SearchInput'
+}
+</script>
+
+<style scoped>
+.visually-hidden {
+  position: absolute;
+  width: 1px;
+  height: 1px;
+  padding: 0;
+  margin: -1px;
+  overflow: hidden;
+  clip: rect(0, 0, 0, 0);
+  white-space: nowrap;
+  border: 0;
+}
+</style>
