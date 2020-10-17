5a7822fdc4683703780c6ad96d60090250dc9f8e
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-10-17 18:26:54 +0200

    Update ShowsListItem component style

--- src/components/ShowsListItem.vue
+++ src/components/ShowsListItem.vue
@@ -1,8 +1,12 @@
 <template>
-  <div class="shows-list-item">
-    <h2>{{show.name}}</h2>
-    <p>premiered: {{show.premiered}}</p>
-    <span>rating: {{show.rating}} / 10</span>
+  <div class="shows-list-item" :style="{backgroundImage: `url(${show.imgURL})`}" @click="isSummaryShown = !isSummaryShown">
+    <div class="info-wrapper">
+      <h2 class="title">{{show.name}}</h2>
+      <div class="year-rating-wrapper">
+        <div class="year">{{show.premiered}}</div>
+        <div class="rating">{{show.rating}}</div>
+      </div>
+    </div>
   </div>
 </template>
 
@@ -15,25 +19,46 @@ export default {
         name: 'True Detective', // tutaj tytuł filmu
         premiered: 2014, // rok premiery
         rating: 10, // ocena, 0 - 10
-      }
+        imgURL: 'http://static.tvmaze.com/uploads/images/medium_portrait/178/445621.jpg',
+      },
     }
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
 .shows-list-item {
+  text-align: center;
   display: flex;
-  flex-direction: column;
+  flex-direction: column-reverse;
   height: 300px;
   width: 200px;
-  background: #efefef;
-
-  text-align: center;
-  margin: 10px;
+  margin: 1vw;
+  background: rgb(119, 7, 7);
+  & .info-wrapper {
+    background-color: black;
+    color: white;
+    & h2 {
+      margin: 5px 0 0;
+    }
+    & .year-rating-wrapper {
+      display: flex;
+      flex-direction: row;
+      justify-content: space-around;
+    }
+    & .rating {
+      color: rgb(236, 213, 4);
+      transform: scale(1.1);
+      font-weight: bold;
+    }
+  }
 }
-p, span {
-  background: #000;
-  color: #fff;
+
+.summary {
+  height: 100%;
+  width: 100%;
+  background: rgba(255, 255, 255, 0.6);
+  display: flex;
+  align-items: center;
 }
 </style>
