59fe9532b9ae6a3f581cda47cdd077dd1c4b13fd
Jakub Jan Strebeyko <strebeyko@gmx.com>
2020-10-17 20:56:58 +0200

    Create ShowsList.vue

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,65 @@
 <template>
   <div id="app">
-    <div class="shows-list">
-      <ShowsListItem :show="show"/>
-    </div>
+    <ShowsList :shows="shows"/>
   </div>
 </template>
 ------------------------------------------------------
 <script>
-import ShowsListItem from '@/components/ShowsListItem'
+import ShowsList from '@/components/ShowsList'
 
 export default {
   name: 'App',
   components: {
-    ShowsListItem
+    ShowsList
   },
   data() {
     return {
-      show: {
-        name: 'True Detective', // tutaj tytuł filmu
-        premiered: 2014, // rok premiery
-        rating: 10, // ocena, 0 - 10
-        imgURL: 'http://static.tvmaze.com/uploads/images/medium_portrait/178/445621.jpg',
-        summary: 'Tak, to jest po prostu świetny serial, no. Bardzo polecam',
-      },
+      shows: [
+        {
+          name: 'True Detective',
+          premiered: 2014,
+          rating: 10,
+          imgURL: 'http://static.tvmaze.com/uploads/images/medium_portrait/178/445621.jpg',
+          summary: 'Tak, to jest po prostu świetny serial, no. Bardzo polecam',
+        },
+        {
+          name: 'House Of Cards',
+          premiered: 2013,
+          rating: 10,
+          imgURL: 'http://static.tvmaze.com/uploads/images/medium_portrait/169/424482.jpg',
+          summary: 'Ojej, no ten to wyjątkowo, taki Thriller Polityczny najwyższych lotów.',
+        },
+        {
+          name: 'Berserk',
+          premiered: 1997,
+          rating: 9,
+          imgURL: 'http://static.tvmaze.com/uploads/images/medium_portrait/0/249.jpg',
+          summary: 'Kolega mówi, że to świetne animbo, ale trochę się cykam',
+        },
+        {
+          name: 'Rick and Morty',
+          premiered: 2013,
+          rating: 9,
+          imgURL: 'http://static.tvmaze.com/uploads/images/medium_portrait/1/3603.jpg',
+          summary: 'A to jest o takim tym, dziadku co jest ogórkiem czy coś',
+        },
+        {
+          name: 'Conan',
+          premiered: 2010,
+          rating: 8,
+          imgURL: 'http://static.tvmaze.com/uploads/images/medium_portrait/1/4583.jpg',
+          summary: 'Śmieszny pan taki.',
+        },
+        {
+          name: 'The Daily Show with Jon Stewart',
+          premiered: 1999,
+          rating: 10,
+          imgURL: 'http://static.tvmaze.com/uploads/images/medium_portrait/1/4598.jpg',
+          summary: 'Inny śmieszny Pan',
+        },
+      ]
     }
-  }
+  },
   // methods: {},
   // computed: {},
   // watch: {},
@@ -49,8 +84,6 @@ body {
   color: #2c3e50;
   height: 100vh;
   width: 100vw;
-  .shows-list {
-    padding: 15px;
-  }
+  display: flex;
 }
 </style>
---
+++ src/components/ShowsList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="shows-list">
+    <ShowsListItem :show="show" v-for="show in shows" :key="show.name"/>
+  </div>
+</template>
+
+<script>
+import ShowsListItem from '@/components/ShowsListItem'
+
+export default {
+  name: 'ShowsList',
+  props: ['shows'],
+  components: {
+    ShowsListItem
+  },
+}
+</script>
+
+<style scoped>
+.shows-list {
+  padding: 15px;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: #2c0606;
+  flex-grow:1;
+}
+</style>
